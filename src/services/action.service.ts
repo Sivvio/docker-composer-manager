a005bd61af23e0871e49d52572759a16030c498c
silvio_piccolo <silvio_piccolo@calypso.com>
2020-03-02 15:54:59 +0000

    fixed spin up images
    Change-Id: I3e42a4cbc06753249eb7f2b00752e97f18cf41be

--- src/services/action.service.ts
+++ src/services/action.service.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs';
 import { ApplicationStatus } from "../models/application-status";
 import { Util } from '../util/util';
 import { Socket } from 'socket.io';
+import { PrivateKeyInput } from 'crypto';
+import { setInterval } from 'timers';
 const Tail = require('tail').Tail;
 
 
@@ -10,89 +12,92 @@ export class ActionService {
     constructor(private applicationStatus: ApplicationStatus) { }
 
     spinUpImages(actions): Promise<any> {
+        const selectedActions = Array.isArray(Object.values(actions)[0]) ? Object.values(actions)[0] : [Object.values(actions)][0];
+
+        const executedAll: Promise<any>[] = [];
+
+        //@ts-ignore
+        selectedActions.forEach(a => {
+            executedAll.push(this.actionToPromise(a));
+        });
+
+        return Promise.all(executedAll);
+    }
+
+    private actionToPromise(a: string): Promise<any> {
         return new Promise(resolve => {
-            const selectedActions = Array.isArray(Object.values(actions)[0]) ? Object.values(actions)[0] : Object.values(actions);
 
-            const commandsToPerform = [];
+            let errorPromise: Promise<Map<string, string>>;
 
-            let errorPromises: Promise<Map<string, string>>[] = [];
+            const command = this.applicationStatus.config.commandsList.get(a);
 
-            //@ts-ignore
-            selectedActions.forEach(a => {
-                commandsToPerform.push(this.applicationStatus.config.commandsList.get(a));
-            });
+            const serviceName = command.slice(command.lastIndexOf(' ') + 1);
+            const fileLogName = this.applicationStatus.config.dockerLogsFolder + serviceName;
+            const runningServiceProcess = Util.spawn(command);
 
-            commandsToPerform.forEach((c) => {
-                const serviceName = c.slice(c.lastIndexOf(' ') + 1);
-                const fileLogName = this.applicationStatus.config.dockerLogsFolder + serviceName;
-                const runningServiceProcess = Util.spawn(c);
+            Util.tailLogsToFile(fileLogName, runningServiceProcess);
 
-                Util.tailLogsToFile(fileLogName, runningServiceProcess);
+            //prepare a promise for the error to be displayed
+            errorPromise = new Promise(res => {
+                let errorMsg = '';
+                let error: Map<string, string> = new Map<string, string>();
 
-                //prepare a promise for the error to be displayed
-                let errorPromise = new Promise(res => {
-                    let errorMsg = '';
-                    let error: Map<string, string> = new Map<string, string>();
-
-                    runningServiceProcess.stderr.on("data", (data) => {
-                        errorMsg += data.toString();
-                    });
-
-                    runningServiceProcess.stderr.on("end", (data) => {
-                        error.set(serviceName, errorMsg);
-                    });
-
-                    runningServiceProcess.stdout.on("close", (data) => {
-                        res(error);
-                    });
+                runningServiceProcess.stderr.on("data", (data) => {
+                    errorMsg += data.toString();
                 });
 
-                //@ts-ignore
-                errorPromises.push(errorPromise);
+                runningServiceProcess.stderr.on("end", (data) => {
+                    error.set(serviceName, errorMsg);
+                });
 
-                //saving the process into a map, we can kill the process on altImages
-                this.applicationStatus.runningServicesProcesses.set(serviceName, runningServiceProcess);
-
-                // we set the terminal id and the name of the running service associated with the terminal
-                this.applicationStatus.runningImages.add(serviceName);
+                runningServiceProcess.stdout.on("close", (data) => {
+                    res(error);
+                });
             });
 
-            // set the services to be active for the UI
-            this.applicationStatus.commands.cmd.forEach(c => {
-                this.applicationStatus.runningImages.forEach((value) => {
-                    if (c.serviceName === value) {
-                        c.active = true;
+            //saving the process into a map, we can kill the process on altImages
+            this.applicationStatus.runningServicesProcesses.set(serviceName, runningServiceProcess);
+
+            // set running images, will confirm later if they ar actually running with docker ps
+            this.applicationStatus.runningImages.add(serviceName);
+
+            let uiStatus = this.applicationStatus.commands.cmd.filter(c => c.serviceName === a)[0];
+
+            // performing a docker ps on the specific instance to check if it actually got started, try 
+            let numberOfTrials = 0;
+
+            let trial;
+            let psOutput: string;
+            let psOutputArray: string[];
+
+            // try to docker ps 5 times if there's no result, fail
+            if (numberOfTrials <= 5) {
+                trial = setInterval(() => {
+                    psOutput = Util.executeBashCommand('docker ps --filter name=' + uiStatus.serviceName);
+                    psOutputArray = psOutput.split(" ");
+                    psOutputArray = psOutputArray.filter(out => out.trim() === uiStatus.serviceName);
+
+                    if (psOutputArray.length >= 1) {
+                        uiStatus.active = true;
+                        uiStatus.errorMessage = null;
+                        clearInterval(trial);
+                        resolve();
                     }
-                    resolve();
-                    //@TODO improve this bit, until then commented out
-                    //performing a docker ps on the specific instance to check if it actually got started
-                    // const psOutput: string = Util.executeBashCommand('docker ps --filter name=' + c.serviceName);
-                    // let psOutputArray: string[] = psOutput.split(" ");
-                    // psOutputArray = psOutputArray.filter(out => out.trim() === c.serviceName);
-                    //
-                    // if (c.serviceName === value && psOutputArray.length === 1) {
-                    //     c.active = true;
-                    //     c.errorMessage = null;
-                    //     resolve();
-                    // } else if (c.serviceName === value) {
-                    //     Promise.all(errorPromises)
-                    //         .then(array => {
-                    //             c.active = false;
-                    //             array.forEach(el => {
-                    //                 let err = el ? el.get(c.serviceName) : null;
-                    //                 if (err) {
-                    //                     c.errorMessage = err;
-                    //                 }
-                    //             });
-                    //             resolve();
-                    //         })
-                    //         .catch(err => console.log(err))
-                    // }
-                });
 
-            });
+                    numberOfTrials++;
+                    console.log('Attempting docker ps for service : ' + uiStatus.serviceName + ' - ' + numberOfTrials);
+                }, 1000);
+            } else {
+                uiStatus.active = false;
+                errorPromise.then(err => {
+                    uiStatus.errorMessage = err.get(uiStatus.serviceName);
+                    resolve();
+                });
+            }
         });
     }
+
+
 
     altImages(serviceName): void {
 
@@ -118,26 +123,26 @@ export class ActionService {
     }
 
     tailLogs(serviceName: string, socket: Socket, fullLogs = false): void {
-            const fileLogName = this.applicationStatus.config.dockerLogsFolder + serviceName + '.log';
-            let tail;
-            var options = { separator: /[\r]{0,1}\n/, fromBeginning: fullLogs, fsWatchOptions: {}, follow: true, useWatchFile: true }
-    
-            try {
-                tail = new Tail(fileLogName, options);
-                this.applicationStatus.runningTails.set(socket.id, { tail: tail, isFull: fullLogs });
-            } catch (e) {
-                console.log(e);
+        const fileLogName = this.applicationStatus.config.dockerLogsFolder + serviceName + '.log';
+        let tail;
+        var options = { separator: /[\r]{0,1}\n/, fromBeginning: fullLogs, fsWatchOptions: {}, follow: true, useWatchFile: true }
+
+        try {
+            tail = new Tail(fileLogName, options);
+            this.applicationStatus.runningTails.set(socket.id, { tail: tail, isFull: fullLogs });
+        } catch (e) {
+            console.log(e);
+        }
+
+        tail.on('line', (chunk) => {
+            if (chunk) {
+                socket.emit('log-stream', chunk);
             }
-    
-            tail.on('line', (chunk) => {
-                if (chunk) {
-                    socket.emit('log-stream', chunk);
-                }
-            });
-    
-            tail.on('error', (err) => {
-                console.log(err);
-            });      
+        });
+
+        tail.on('error', (err) => {
+            console.log(err);
+        });
     }
 
     streamFullLogs(serviceName: string, socket: Socket): void {
